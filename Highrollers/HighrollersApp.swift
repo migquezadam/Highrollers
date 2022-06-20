869f860decac7c7e38954e4dc81fda8019f4bcf6
Miguel Quezada <mig.aquezada@gmail.com>
2022-06-19 22:40:03 -0400

    Initial Commit

---
+++ Highrollers/HighrollersApp.swift
@@ -0,0 +1,17 @@
+//
+//  HighrollersApp.swift
+//  Highrollers
+//
+//  Created by Miguel Quezada on 19-06-22.
+//
+
+import SwiftUI
+
+@main
+struct HighrollersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
